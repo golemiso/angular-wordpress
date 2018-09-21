516bad55f2e6cd66bb428859fe6cb23e40bd95c4
golemiso <golemiso@users.noreply.github.com>
2018-09-21 19:59:46 +0900

    modify routing

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -3,13 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { FrontComponent } from '../pages/front/front.component';
 
 const routes: Routes = [
-  { path: 'front', component: FrontComponent }
+  { path: '', component: FrontComponent },
+  { path: ':slug', component: FrontComponent }
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes)
-  ],
-  declarations: []
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class RoutingModule { }
