3b96ab04f6c07dad8a835b2215520f3ec8f96ea8
golemiso <golemiso@users.noreply.github.com>
2018-09-22 17:59:21 +0900

    article list should show excerpt

--- src/app/models/article.ts
+++ src/app/models/article.ts
@@ -1,5 +1,5 @@
 export interface Article {
   slug: string;
   title: { rendered: string; };
-  content: { rendered: string; };
+  excerpt: { rendered: string; };
 }
--- src/app/wordpress.service.ts
+++ src/app/wordpress.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 import { Observable, of } from 'rxjs';
 
@@ -7,9 +7,11 @@ import { Article } from './models/article';
 
 const httpOptions = {
   headers: new HttpHeaders({
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*'
-  })
+    'Content-Type': 'application/json'
+  }),
+  // params: {
+  //   'context': 'embed'
+  // }
 };
 
 @Injectable({
@@ -21,7 +23,7 @@ export class WordpressService {
 
   fetchArticles(): Observable<Array<Article>> {
 
-    return this.http.get<Array<Article>>("https://demo.wp-api.org/wp-json/wp/v2/posts")
+    return this.http.get<Array<Article>>("https://demo.wp-api.org/wp-json/wp/v2/posts", httpOptions)
     // const article = [{ slug: "slug", title: "title", content: "content" }];
     // return of(article);
   }
