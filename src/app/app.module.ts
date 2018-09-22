376cb11ff0aaded3000d3fab94be5fdbf69efdb7
golemiso <golemiso@users.noreply.github.com>
2018-09-22 14:53:21 +0900

    refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule, APP_INITIALIZER } from '@angular/core';
 
 import { AppConfig } from './app.config';
 import { AppComponent } from './app.component';
-import { RoutingModule } from './routing/routing.module';
 import { FrontComponent } from './pages/front/front.component';
 import { ArticleComponent } from './pages/article/article.component';
+
+import { RoutingModule } from './routing/routing.module';
 
 export function initializeConfig(config: AppConfig) {
   return () => config.load();
