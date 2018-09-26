9f5a5a52132b05e83991fd12a5be13c7957472ed
golemiso <golemiso@users.noreply.github.com>
2018-09-26 10:43:59 +0900

    add navigator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule, APP_INITIALIZER } from '@angular/core';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { LayoutModule } from '@angular/cdk/layout';
+
 
 import { AppConfig } from './app.config';
 import { AppComponent } from './app.component';
 import { FrontComponent } from './pages/front/front.component';
 import { ArticleComponent } from './pages/article/article.component';
+import { NavComponent } from './nav/nav.component';
 
 import { RoutingModule } from './routing/routing.module';
-
 export function initializeConfig(config: AppConfig) {
   return () => config.load();
 }
@@ -17,12 +21,20 @@ export function initializeConfig(config: AppConfig) {
   declarations: [
     AppComponent,
     FrontComponent,
-    ArticleComponent
+    ArticleComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    RoutingModule
+    RoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [
     AppConfig,
